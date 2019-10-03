46652e777f462e8ce54a0641d1fb1a6adf189752
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-10-03 03:39:38 -0400

    Updates comments in colors.js file

--- colors.js
+++ colors.js
@@ -1,20 +1,21 @@
 module.exports = {
-  /* white; links in top navBar */
-  background: "#FFFFFF",
-  /* light cyan; middle triangle in "Landing" section */
-  backgroundDark: "#c4fffd",
-  /* royal blue; title text in "Landing" section */
-  primary: "#3257fa",
-  /* dark violet; used to underline text (i.e. - section headers) */
-  primaryLight: "#7c00e7",
-  /* dark blue; top-right triangle in "Landing" section + sticky navBar */
-  primaryDark: "#3257fa",
-  /* deep sky blue; top-left triangle in "About Me" section */
-  secondary: "#05d1ff",
-  /* deep sky blue; stripe between "About Me" & "Projects" section */
-  secondaryLight: "#05d1ff",
-  /* dark blue; top-right triangle in "Landing" section + sticky navBar */
-  secondaryDark: "#3257fa",
+  /*------------------Global Color Variables-----------------*/
+  /* links in the top navBar */
+  background: "#FFFFFF" /* white */,
+  /* middle triangle in "Landing" section */
+  backgroundDark: "#c4fffd" /* light cyan */,
+  /* title text in "Landing" section */
+  primary: "#3257fa" /* royal blue */,
+  /* used to underline text (i.e. - section headers) */
+  primaryLight: "#7c00e7" /* dark violet */,
+  /* top-right triangle in "Landing" section + sticky navBar */
+  primaryDark: "#3257fa" /* royal blue */,
+  /* top-left triangle in "About Me" section */
+  secondary: "#05d1ff" /* deep sky blue */,
+  /* stripe between "About Me" & "Projects" section */
+  secondaryLight: "#05d1ff" /* deep sky blue */,
+  /* top-right triangle in "Landing" section + sticky navBar */
+  secondaryDark: "#3257fa" /* royal blue */,
   /*------------------Components-----------------*/
   /* Card.js */
   card_bg: "#FFFFFF" /* white */,
